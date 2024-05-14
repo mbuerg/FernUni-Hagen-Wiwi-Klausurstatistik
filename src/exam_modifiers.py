a93c3f37b6f4d8ae3bf7113e632c20e911d5c815
mbuerg <106337257+mbuerg@users.noreply.github.com>
2024-05-14 17:48:15 +0200

    fill semester Funktion

--- src/exam_modifiers.py
+++ src/exam_modifiers.py
@@ -26,5 +26,49 @@ def sort_by_semester(klausurdaten: "pd.DataFrame"):
     del klausurdaten ["Jahr"]
     
 
+def fill_missing_semester(klausurdaten: pd.DataFrame, letztes_jahr: int) -> pd.DataFrame:
+    semester_vorhanden = klausurdaten["Semester"].unique()
+    semester_ideal = []
+    for i in np.arange(2011, letztes_jahr+1):
+        for j in ["Sommersemester ", "Wintersemester "]:
+            semester_ideal.append(f"{j}{i}")
+    
+    semester_fehlend = np.setdiff1d(semester_ideal, semester_vorhanden, assume_unique=True)
+    
+    klausurdaten_update = klausurdaten.copy()
+    for i, j in enumerate(semester_fehlend):
+        
+        semester_fehlend_index = semester_ideal.index(semester_fehlend[i])
+        
+        semester_fehlend_vorher = semester_ideal[semester_fehlend_index-1]
+        semester_fehlend_nachher = semester_ideal[semester_fehlend_index+1]
+    
+        module_fehlend_vorher = klausurdaten_update.query("Semester == @semester_fehlend_vorher")["Modulnummer"].unique()
+        module_fehlend_nachher = klausurdaten_update.query("Semester == @semester_fehlend_nachher")["Modulnummer"].unique()
+    
+        module_fehlendes_semester = list(np.intersect1d(module_fehlend_vorher, module_fehlend_nachher, assume_unique=True))
+        
+        klausurdaten_gefiltert_vorher = (klausurdaten_update
+        .query("Semester == @semester_fehlend_vorher & Modulnummer == @module_fehlendes_semester"))
 
+        klausurdaten_gefiltert_nachher = (klausurdaten_update
+        .query("Semester == @semester_fehlend_nachher & Modulnummer == @module_fehlendes_semester"))
 
+        klausurdaten_fehlendes_semester = (pd.concat([klausurdaten_gefiltert_vorher
+                                            , klausurdaten_gefiltert_nachher])
+                                    .groupby(["Modulname"
+                                                , "Modulnummer"])[["Teilnehmer"
+                                                            , "sehr gut"
+                                                            , "gut"
+                                                            , "befriedigend"
+                                                            , "ausreichend"
+                                                            , "nicht ausreichend"]]
+                                    .mean()
+                                    .reset_index())
+
+        klausurdaten_fehlendes_semester.insert(2, "Semester", j)
+
+        klausurdaten_update = pd.concat([klausurdaten_update, klausurdaten_fehlendes_semester]
+                                , ignore_index=True)
+    
+    return klausurdaten_update
--- src/main.py
+++ src/main.py
@@ -8,9 +8,11 @@ import exam_modifiers
 def main():
     soup, buttons = scraper.scrape()
     klausurdaten = exam_dataframe.build_dataframe(soup, buttons)
-    exam_modifiers.berechne_durchschnittsnote(klausurdaten)
-    exam_modifiers.sort_by_semester(klausurdaten)
-    klausurdaten.to_csv("../data/klausurdaten.csv", index=False)
+    klausurdaten_gesamt = exam_modifiers.fill_missing_semester(klausurdaten=klausurdaten
+                                                               , letztes_jahr= 2023)
+    exam_modifiers.berechne_durchschnittsnote(klausurdaten_gesamt)
+    exam_modifiers.sort_by_semester(klausurdaten_gesamt)
+    klausurdaten_gesamt.to_csv("../data/klausurdaten.csv", index=False)
 
 
 if __name__ == "__main__":

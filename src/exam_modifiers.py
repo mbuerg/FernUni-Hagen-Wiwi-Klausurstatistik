6307249ea159be27d94e0835ff16b5bc71b817ba
mbuerg <106337257+mbuerg@users.noreply.github.com>
2024-05-14 18:14:59 +0200

    main update

--- src/exam_modifiers.py
+++ src/exam_modifiers.py
@@ -1,30 +1,38 @@
 import numpy as np
 import pandas as pd
 
-def berechne_durchschnittsnote(klausurdaten: "pd.DataFrame"):
+def berechne_durchschnittsnote(klausurdaten: pd.DataFrame) -> pd.DataFrame:
     
-    klausurdaten["Durchschnittsnote"] = klausurdaten.iloc[:, 3:9].apply(lambda x: np.sum(x.iloc[1:6] * np.arange(1,6))/x.iloc[0], axis=1)
-    klausurdaten["Durchschnittsnote"] = np.round(klausurdaten["Durchschnittsnote"], 4)
+    klausurdaten_durchschnitt = klausurdaten.copy()
+    klausurdaten_durchschnitt["Durchschnittsnote"] = klausurdaten_durchschnitt.iloc[:, 3:9].apply(lambda x: np.sum(x.iloc[1:6] * np.arange(1,6))/x.iloc[0], axis=1)
+    klausurdaten_durchschnitt["Durchschnittsnote"] = np.round(klausurdaten_durchschnitt["Durchschnittsnote"], 4)
 
-def sort_by_semester(klausurdaten: "pd.DataFrame"):
+    return klausurdaten_durchschnitt
+
+
+
+
+
+def sort_by_semester(klausurdaten: pd.DataFrame) -> pd.DataFrame:
     
-    klausurdaten["Jahr"] = klausurdaten["Semester"].apply(lambda x: x[-4:])
-    klausurdaten["Semester"] = klausurdaten["Semester"].str.replace(
+    klausurdaten_sortiert = klausurdaten.copy()
+    klausurdaten_sortiert["Jahr"] = klausurdaten_sortiert["Semester"].apply(lambda x: x[-4:])
+    klausurdaten_sortiert["Semester"] = klausurdaten_sortiert["Semester"].str.replace(
         r"Wintersemester \d{4}", "WS", regex = True)
-    klausurdaten["Semester"] = klausurdaten["Semester"].str.replace(
+    klausurdaten_sortiert["Semester"] = klausurdaten_sortiert["Semester"].str.replace(
         r"Sommersemester \d{4}", "SS", regex = True)
 
     # dataframe sortieren
-    klausurdaten.sort_values(["Modulnummer", "Jahr", "Semester"], inplace = True)
+    klausurdaten_sortiert.sort_values(["Modulnummer", "Jahr", "Semester"], inplace = True)
 
     # Semesterdaten wieder zusammenfügen
-    klausurdaten["Semester"] = klausurdaten["Semester"] + klausurdaten["Jahr"]
-    #klausurdaten["Jahreszähler"] = str(klausurdaten["Jahr"]) + klausurdaten["Semester"].str[:2]
-    #klausurdaten["Jahreszähler"] = klausurdaten["Jahreszähler"].str.replace("WS", "01")
-    #klausurdaten["Jahreszähler"] = klausurdaten["Jahreszähler"].str.replace("SS", "02")
-    #klausurdaten["Jahreszähler"] = klausurdaten["Jahreszähler"].astype("int32")
-    del klausurdaten ["Jahr"]
+    klausurdaten_sortiert["Semester"] = klausurdaten_sortiert["Semester"] + klausurdaten_sortiert["Jahr"]
+    del klausurdaten_sortiert["Jahr"]
     
+    return klausurdaten_sortiert
+
+
+
 
 def fill_missing_semester(klausurdaten: pd.DataFrame, letztes_jahr: int) -> pd.DataFrame:
     semester_vorhanden = klausurdaten["Semester"].unique()
@@ -35,7 +43,7 @@ def fill_missing_semester(klausurdaten: pd.DataFrame, letztes_jahr: int) -> pd.D
     
     semester_fehlend = np.setdiff1d(semester_ideal, semester_vorhanden, assume_unique=True)
     
-    klausurdaten_update = klausurdaten.copy()
+    klausurdaten_filled = klausurdaten.copy()
     for i, j in enumerate(semester_fehlend):
         
         semester_fehlend_index = semester_ideal.index(semester_fehlend[i])
@@ -43,15 +51,15 @@ def fill_missing_semester(klausurdaten: pd.DataFrame, letztes_jahr: int) -> pd.D
         semester_fehlend_vorher = semester_ideal[semester_fehlend_index-1]
         semester_fehlend_nachher = semester_ideal[semester_fehlend_index+1]
     
-        module_fehlend_vorher = klausurdaten_update.query("Semester == @semester_fehlend_vorher")["Modulnummer"].unique()
-        module_fehlend_nachher = klausurdaten_update.query("Semester == @semester_fehlend_nachher")["Modulnummer"].unique()
+        module_fehlend_vorher = klausurdaten_filled.query("Semester == @semester_fehlend_vorher")["Modulnummer"].unique()
+        module_fehlend_nachher = klausurdaten_filled.query("Semester == @semester_fehlend_nachher")["Modulnummer"].unique()
     
         module_fehlendes_semester = list(np.intersect1d(module_fehlend_vorher, module_fehlend_nachher, assume_unique=True))
         
-        klausurdaten_gefiltert_vorher = (klausurdaten_update
+        klausurdaten_gefiltert_vorher = (klausurdaten_filled
         .query("Semester == @semester_fehlend_vorher & Modulnummer == @module_fehlendes_semester"))
 
-        klausurdaten_gefiltert_nachher = (klausurdaten_update
+        klausurdaten_gefiltert_nachher = (klausurdaten_filled
         .query("Semester == @semester_fehlend_nachher & Modulnummer == @module_fehlendes_semester"))
 
         klausurdaten_fehlendes_semester = (pd.concat([klausurdaten_gefiltert_vorher
@@ -68,7 +76,7 @@ def fill_missing_semester(klausurdaten: pd.DataFrame, letztes_jahr: int) -> pd.D
 
         klausurdaten_fehlendes_semester.insert(2, "Semester", j)
 
-        klausurdaten_update = pd.concat([klausurdaten_update, klausurdaten_fehlendes_semester]
+        klausurdaten_filled = pd.concat([klausurdaten_filled, klausurdaten_fehlendes_semester]
                                 , ignore_index=True)
     
-    return klausurdaten_update
+    return klausurdaten_filled
--- src/main.py
+++ src/main.py
@@ -8,11 +8,11 @@ import exam_modifiers
 def main():
     soup, buttons = scraper.scrape()
     klausurdaten = exam_dataframe.build_dataframe(soup, buttons)
-    klausurdaten_gesamt = exam_modifiers.fill_missing_semester(klausurdaten=klausurdaten
+    klausurdaten_filled = exam_modifiers.fill_missing_semester(klausurdaten=klausurdaten
                                                                , letztes_jahr= 2023)
-    exam_modifiers.berechne_durchschnittsnote(klausurdaten_gesamt)
-    exam_modifiers.sort_by_semester(klausurdaten_gesamt)
-    klausurdaten_gesamt.to_csv("../data/klausurdaten.csv", index=False)
+    klausurdaten_filled_average = exam_modifiers.berechne_durchschnittsnote(klausurdaten_filled)
+    klausurdaten_filled_average_sortiert = exam_modifiers.sort_by_semester(klausurdaten_filled_average)
+    klausurdaten_filled_average_sortiert.to_csv("../data/klausurdaten.csv", index=False)
 
 
 if __name__ == "__main__":
